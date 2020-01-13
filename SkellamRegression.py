1f3d8b659d0f5360362fa167f6b0d8a23f35a5c5
nathan <nathan@swipejobs.com>
2020-01-14 09:39:15 +1100

    update

--- SkellamRegression.py
+++ SkellamRegression.py
@@ -14,14 +14,14 @@ class SkellamRegression:
     def log_likelihood(self, coefficients):
         coefficients1 = coefficients[0:len(coefficients) // 2]
         coefficients2 = coefficients[len(coefficients) // 2:]
-        l1 = self.X @ coefficients1
-        l2 = self.X @ coefficients2
-        neg_log_likelihood = -np.sum(skellam.logpmf(self.y, mu1=np.exp(l1), mu2=np.exp(l2), loc=0))
+        lambda1 = self.x @ coefficients1
+        lambda2 = self.x @ coefficients2
+        neg_log_likelihood = -np.sum(skellam.logpmf(self.y, mu1=np.exp(lambda1), mu2=np.exp(lambda2), loc=0))
         return neg_log_likelihood
 
     def train(self):
         # initial estimate
-        x0 = np.ones(self.X.shape[1] * 2)
+        x0 = np.ones(self.x.shape[1] * 2)
 
         results = minimize(self.log_likelihood,
                            x0,
